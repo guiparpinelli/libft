b7baa1ba8221be45fcd133021730365c08800895
Guilherme Parpinelli <guilheerme.p@gmail.com>
2023-04-14 16:38:38 +0100

    format and add header

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gparpine <gparpine@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 16:22:52 by gparpine          #+#    #+#             */
+/*   Updated: 2023/04/14 16:23:32 by gparpine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <stdlib.h>
 
-char *ft_strdup(const char *s1) {
-  char *result;
-  size_t len;
+char	*ft_strdup(const char *s1)
+{
+	char	*result;
+	size_t	len;
 
-  len = ft_strlen(s1);
-  result = (char *)malloc(sizeof(char) * (len + 1));
-  if (!result)
-    return (NULL);
-  result[len] = '\0';
-  while (len >= 0) {
-    result[len] = s1[len];
-    len--;
-  }
-  return (result);
+	len = ft_strlen(s1);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	while (len >= 0)
+	{
+		result[len] = s1[len];
+		len--;
+	}
+	return (result);
 }
