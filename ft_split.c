85fc72200eccac725729c1d4181b146969e800a3
Guilherme Parpinelli <gparpine@student.42porto.com>
2023-04-24 17:59:28 +0100

    wip

--- ft_split.c
+++ ft_split.c
@@ -6,10 +6,37 @@
 /*   By: gparpine <gparpine@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:52:41 by gparpine          #+#    #+#             */
-/*   Updated: 2023/04/24 16:09:23 by gparpine         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:58:47 by gparpine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	count_words(char *str, char c)
+{
+	int	num_words;
+	int	flag;
+
+	num_words = 0;
+	flag = 0;
+	while (*str)
+	{
+		if (*str != c && flag == 0)
+		{
+			num_words++;
+			flag = 1;
+		}
+		else if (*str == c)
+			flag = 0;
+		str++;
+	}
+	return (num_words);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		num_words;
+	char	**words;
+
+	num_words = count_words(str);
 }
